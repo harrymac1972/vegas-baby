8b1569f2dae07b04c2fd2bb69d3458681530cc7d
harry <harrymac1972@gmail.com>
2023-11-15 07:17:41 -0700

    upgrade button added

--- assets/js/form.js
+++ assets/js/form.js
@@ -50,24 +50,27 @@ function _init() {
     emailDiv.append(emailInp);
     detailsDiv.append(emailDiv);
 
-    var interestsArr = renderQuestions();
+    var basicInterestsArr = renderBasicQuestions();
+    if (packageType === "premium") {
+        var premiumInterestsArr = renderPremiumQuestions();
+    }
 
-    buttonsRender(interestsArr);
+    buttonsRender(basicInterestsArr,premiumInterestsArr);
 }
 
-function buttonsRender(interestsArr) {
+function buttonsRender(basicInterestsArr,premiumInterestsArr) {
     var resetBtn = $("<button>");
     resetBtn.attr("id","reset-btn");
     resetBtn.text("Reset");
     resetBtn.on("click",function() {
-        formReset(interestsArr);
+        formReset(basicInterestsArr);
     })
     
     var submitBtn = $("<button>");
     submitBtn.attr("id","submit-btn");
     submitBtn.text("Submit");
     submitBtn.on("click",function() {
-        formSubmit(interestsArr);
+        formSubmit(basicInterestsArr);
     })
 
     var btnDiv = $('<div>');
@@ -79,28 +82,28 @@ function buttonsRender(interestsArr) {
 
 }
 
-function formReset(interestsArr) {
+function formReset(basicInterestsArr) {
     $("#name-inp").val("");
     $("#email-inp").val("");
-    for (var i=0; i<interestsArr.length; i++) {
+    for (var i=0; i<basicInterestsArr.length; i++) {
         var id = "#input-" + i;
         $(id).prop('checked', false);
     }
 }
 
-function formSubmit(interestsArr) {
+function formSubmit(basicInterestsArr) {
     storageObj = {};
     storageObj["fullName"] = $("#name-inp").val();
     storageObj["email"] = $("#email-inp").val();
     storageObj["questionsObj"] = {};
-    for (var i=0; i<interestsArr.length; i++) {
+    for (var i=0; i<basicInterestsArr.length; i++) {
         var id = "#input-" + i;
         if ($(id).is(":checked")) {
             var bool = 1;
         } else {
             var bool = 0;
         }
-        storageObj["questionsObj"][interestsArr[i]["lbl"]] = bool;
+        storageObj["questionsObj"][basicInterestsArr[i]["lbl"]] = bool;
     }
     console.log(storageObj);
     storageSet(storageObj);
@@ -151,7 +154,7 @@ function getInterestsObjArr() {
 };
 
 
-function renderQuestions() {
+function renderBasicQuestions() {
 
     var questionsDiv = $("<div>");
     questionsDiv.attr("id","questions-div");
@@ -167,13 +170,24 @@ function renderQuestions() {
     questionsOneDiv.attr("class","questions-group");
     questionsDiv.append(questionsTwoDiv);
 
-    var interestsArr = getInterestsObjArr();
-    for (var i=0; i<interestsArr.length;i++) {
+    var premiumDiv = $('<div>');
+    premiumDiv.attr("id","premium-div");
+    questionsOneDiv.attr("class","questions-group");
+    questionsDiv.append(premiumDiv);
+
+    var upgradeBtn = $("<button>");
+    upgradeBtn.attr("id","upgrade-btn");
+    // upgradeBtn.attr("class","quest-set");
+    upgradeBtn.text("PREMIUM OPTIONS!");
+    premiumDiv.append(upgradeBtn);
+
+    var basicInterestsArr = getInterestsObjArr();
+    for (var i=0; i<basicInterestsArr.length;i++) {
         var lbl = $("<h4>");
         var id = "quest-" + i;
         lbl.attr("id",id);
         lbl.attr("class","quest-lbl");
-        lbl.text(interestsArr[i]["lbl"]);
+        lbl.text(basicInterestsArr[i]["lbl"]);
           
         var chk = $("<input type='checkbox'>");
         var id = "input-" + i;
@@ -185,13 +199,18 @@ function renderQuestions() {
         questDiv.append(chk);
         questDiv.append(lbl);
 
-        if (i < Math.round(interestsArr.length / 2)) {
+        if (i < Math.round(basicInterestsArr.length / 2)) {
             questionsOneDiv.append(questDiv);
         } else {
             questionsTwoDiv.append(questDiv);
         }
     }
-    return interestsArr;
+    return basicInterestsArr;
+}
+
+function renderPremiumQuestions() {
+    premiumInterestsArr = "[FLESH OUT LATER]";
+    return premiumInterestsArr;
 }
 
 function storageGet() {
