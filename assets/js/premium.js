c8f153e4bfdd7dbc788bc8f5cfb31bc22fe3ae3e
Gym Freak <guminyj@gmail.com>
2023-11-14 21:43:22 -0500

    worked on the carousel but not doing what i want it to going to plus away at it in class

---
+++ assets/js/premium.js
@@ -0,0 +1,7 @@
+const myCarouselElement = document.querySelector("#carousel");
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false,
+  pause: "hover",
+});
